acb27b794c9d6248e44bdba2a95ffba8625d56c5
Kristufoskinas <56268008+Kristufoskinas@users.noreply.github.com>
2025-06-28 23:00:14 +0200

    added input boxes

--- popup.js
+++ popup.js
@@ -7,12 +7,22 @@ document.querySelector(".add").addEventListener("click", () => {
   const nameCol = document.createElement("div");
   nameCol.className = "column";
   nameCol.style.flex = "3";
-  nameCol.textContent = "New Series";
+  // Input the series name
+  const nameInput = document.createElement("input");
+  nameInput.type = "text";
+  nameInput.placeholder = "Series name";
+  nameInput.className = "input-field";
+  nameCol.appendChild(nameInput);
 
   const linkCol = document.createElement("div");
   linkCol.className = "column";
   linkCol.style.flex = "1";
-  linkCol.textContent = "Link";
+  // Input the series link
+  const linkInput = document.createElement("input");
+  const iconButton = document.createElement("button");
+  iconButton.textContent = "↗️";
+  iconButton.className = "icon-button";
+  linkCol.appendChild(iconButton);
 
   row.appendChild(nameCol);
   row.appendChild(linkCol);
